0782388aaafac02ffb5bdeef13353c70facc8c2d
Diego Gobbi Slomp <diegogslomp@gmail.com>
2023-09-12 21:49:06 -0300

    First commit

---
+++ bot.py
@@ -0,0 +1,57 @@
+import asyncio
+import logging
+import sys
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram import F, Router
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import (
+    Message,
+    ReplyKeyboardRemove,
+)
+
+from form import router as form_router
+
+router = Router()
+
+
+@router.message(CommandStart())
+@router.message(F.text.casefold() == "start")
+async def command_start(message: Message, state: FSMContext) -> None:
+    await message.answer(
+        "Main start",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+@router.message(Command("cancel"))
+@router.message(F.text.casefold() == "cancel")
+async def cancel_handler(message: Message, state: FSMContext) -> None:
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    logging.info("Cancelling state %r", current_state)
+    await state.clear()
+    await message.answer(
+        "Cancelled",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+async def main():
+    token = os.getenv("BOT_TOKEN")
+    bot = Bot(token=token, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(form_router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
