b4a2efd4882f4aa0c99947820ac1e55e36fd67e9
Diego Gobbi Slomp <diegogslomp@gmail.com>
2023-09-16 01:26:27 -0300

    Minimal echo initial bot

--- bot.py
+++ bot.py
@@ -5,27 +5,11 @@ import os
 
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
-from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
 
-router = Router()
+echo_router = Router()
 
-
-class States(StatesGroup):
-    some = State()
-
-
-@router.message(Command("cancel"))
-async def cancel_handler(message: types.Message, state: FSMContext) -> None:
-    current_state = await state.get_state()
-    if current_state == None:
-        return
-    await state.clear()
-    await message.answer("Cancelled")
-
-
-@router.message()
+@echo_router.message()
 async def echo_handler(message: types.Message, state: FSMContext) -> None:
     try:
         await message.send_copy(chat_id=message.chat.id)
@@ -37,7 +21,7 @@ async def main() -> None:
     token = os.getenv("BOT_TOKEN")
     bot = Bot(token, parse_mode=ParseMode.HTML)
     dp = Dispatcher()
-    dp.include_router(router)
+    dp.include_router(echo_router)
     await dp.start_polling(bot)
 
 
